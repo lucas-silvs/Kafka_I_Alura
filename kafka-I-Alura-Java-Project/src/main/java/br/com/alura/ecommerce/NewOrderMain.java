b184280f698d88916173486b0205c879203b290d
Lucas da Silva Santos <lucas.parkl@outlook.com>
2022-02-14 18:27:16 -0300

    WIP: Kafka I Alura - finalização do modulo 2:  - execução de mais de um consumer utilizando partição  - parelelização das mensagens consumidas para cada partição, porém ocorre rebalanceamento  - utilização de limitação de mensagens consumidas, evitando o rebalanceamento  - utilização de UUID.randouuid() para que seja feita a paralelização no topico

--- kafka-I-Alura-Java-Project/src/main/java/br/com/alura/ecommerce/NewOrderMain.java
+++ kafka-I-Alura-Java-Project/src/main/java/br/com/alura/ecommerce/NewOrderMain.java
@@ -1,26 +1,33 @@
 package br.com.alura.ecommerce;
 
+import org.apache.kafka.clients.producer.Callback;
 import org.apache.kafka.clients.producer.KafkaProducer;
 import org.apache.kafka.clients.producer.ProducerConfig;
 import org.apache.kafka.clients.producer.ProducerRecord;
 import org.apache.kafka.common.serialization.StringSerializer;
 
 import java.util.Properties;
+import java.util.UUID;
 import java.util.concurrent.ExecutionException;
 
 public class NewOrderMain {
 
     public static void main(String[] args) throws ExecutionException, InterruptedException {
         var  producer = new KafkaProducer<String,String>(properties());
-        var value = "12121,2323232,9000";
-        var record = new ProducerRecord<>("ECOMMERCE_NOVO_PEDIDO",value,value);
-        producer.send(record, (data,ex) -> {
-            if(ex!=null){
+        var key = UUID.randomUUID().toString();
+        var value = key+ " 12121,2323232,9000";
+        var record = new ProducerRecord<>("ECOMMERCE_NOVO_PEDIDO",key,value);
+        Callback callback = (data, ex) -> {
+            if (ex != null) {
                 ex.printStackTrace();
                 return;
             }
-            System.out.println("Sucesso ao enviar no tópico: "+data.topic() + ":::partition " + data.partition() +"/ offset " + data.offset() + "/ time " + data.timestamp());
-        }).get();
+            System.out.println("Sucesso ao enviar no tópico: " + data.topic() + ":::partition " + data.partition() + "/ offset " + data.offset() + "/ time " + data.timestamp());
+        };
+        var email = "Obrigado, estamos processando seu pedido";
+        var emailRecord = new ProducerRecord<>("ECOMMERCE_SEND_EMAIL",key,email);
+        producer.send(record, callback).get();
+        producer.send(emailRecord, callback).get();
     }
 
     private static Properties properties(){
