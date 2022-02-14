7062477e7e95b1a5613cfe974ee90c1a6305beb4
Lucas da Silva Santos <lucas.parkl@outlook.com>
2022-02-14 18:27:16 -0300

    WIP: Kafka I Alura - finalização do modulo 2:  - execução de mais de um consumer utilizando partição  - parelelização das mensagens consumidas para cada partição, porém ocorre rebalanceamento  - utilização de limitação de mensagens consumidas, evitando o rebalanceamento  - utilização de UUID.randouuid() para que seja feita a paralelização no topico

--- kafka-I-Alura-Java-Project/src/main/java/br/com/alura/ecommerce/EmailService.java
+++ kafka-I-Alura-Java-Project/src/main/java/br/com/alura/ecommerce/EmailService.java
@@ -6,46 +6,35 @@ import org.apache.kafka.common.serialization.StringDeserializer;
 
 import java.time.Duration;
 import java.util.Collections;
-import java.util.Map;
 import java.util.Properties;
 
-public class FraudeDetectorService {
+public class EmailService {
     public static void main(String[] args) {
-        var consumer = new KafkaConsumer<String,String>(properties());
-        consumer.subscribe(Collections.singletonList("ECOMMERCE_NOVO_PEDIDO"));
+        var consumer = new KafkaConsumer<String, String>(properties());
+        consumer.subscribe(Collections.singletonList("ECOMMERCE_SEND_EMAIL"));
 
-        while(true) {
+        while (true) {
             var records = consumer.poll(Duration.ofSeconds(1));
             if (!records.isEmpty()) {
                 for (var record : records) {
                     System.out.println("\n");
-                    System.out.println("processando pedidos, necessário criar validação de fraude");
+                    System.out.println("Log");
                     System.out.println("Chave: " + record.key());
                     System.out.println("Valor: " + record.value());
                     System.out.println("Tópico: " + record.topic());
                     System.out.println("offset: " + record.offset());
-
-
-                    try {
-                        Thread.sleep(500);
-                    } catch (InterruptedException e) {
-
-                    }
-                    System.out.println("Pedido processado");
-
                 }
             }
-            System.out.println("Registros vazios");
+
         }
     }
 
     private static Properties properties() {
         var properties = new Properties();
-        properties.setProperty(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG,"127.0.0.1:9092");
+        properties.setProperty(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, "127.0.0.1:9092");
         properties.setProperty(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class.getName());
         properties.setProperty(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class.getName());
-        properties.setProperty(ConsumerConfig.GROUP_ID_CONFIG,FraudeDetectorService.class.getSimpleName());
-        properties.setProperty(ConsumerConfig.MAX_POLL_RECORDS_CONFIG,"1");
+        properties.setProperty(ConsumerConfig.GROUP_ID_CONFIG, EmailService.class.getSimpleName());
 
         return properties;
     }
